b18626f62d0d1e8faed0fe6bf0cc6931363c703e
Vincent Yono <vincentyono@protonmail.com>
2023-09-28 17:45:00 +0700

    Update

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # Computational Biology - Week 3 Individual Assignment
 
 ##### Transcription and Translation
+
+![sample output](https://github.com/vincentyono/comp-bio-week3-assignment/blob/master/img/sample_output.png)
--- main.py
+++ main.py
@@ -43,6 +43,79 @@ def codon_frequency(mrna_seq: str) -> dict:
     return dic
 
 
+def mrna_to_aminoacid(mrna_seq: str) -> str:
+    assert len(mrna_seq) % 3 == 0, "Invalid mRNA sequence length"
+
+    def translate(codon: str) -> str:
+        if codon == "UUU" or codon == "UUC":
+            return "Phe (F)"
+        elif (
+            codon == "UUA"
+            or codon == "UUG"
+            or codon == "CUU"
+            or codon == "CUC"
+            or codon == "CUA"
+            or codon == "CUG"
+        ):
+            return "Leu (L)"
+        elif codon == "AUU" or codon == "AUC" or codon == "AUA":
+            return "Ile (I)"
+        elif codon == "AUG":
+            return "Met (M)"
+        elif codon == "GUU" or codon == "GUC" or codon == "GUA" or codon == "GUG":
+            return "Val (V)"
+        elif (
+            codon == "UCU"
+            or codon == "UCC"
+            or codon == "UCA"
+            or codon == "UCG"
+            or codon == "AGU"
+            or codon == "AGC"
+        ):
+            return "Ser (S)"
+        elif codon == "CCU" or codon == "CCC" or codon == "CCA" or codon == "CCG":
+            return "Pro (P)"
+        elif codon == "ACU" or codon == "ACC" or codon == "ACA" or codon == "ACG":
+            return "Thr (T)"
+        elif codon == "GCU" or codon == "GCC" or codon == "GCA" or codon == "GCG":
+            return "Ala (A)"
+        elif codon == "UAU" or codon == "UAC":
+            return "Tyr (Y)"
+        elif codon == "UAA" or codon == "UAG" or codon == "UGA":
+            return "Stop (*)"
+        elif codon == "CAU" or codon == "CAC":
+            return "His (H)"
+        elif codon == "CAA" or codon == "CAG":
+            return "Gln (Q)"
+        elif codon == "AAU" or codon == "AAC":
+            return "Asn (N)"
+        elif codon == "AAA" or codon == "AAG":
+            return "Lys (K)"
+        elif codon == "GAU" or codon == "GAC":
+            return "Asp (D)"
+        elif codon == "GAA" or codon == "GAG":
+            return "Glu (E)"
+        elif codon == "UGU" or codon == "UGC":
+            return "Cys (C)"
+        elif (
+            codon == "AGA"
+            or codon == "AGG"
+            or codon == "CGU"
+            or codon == "CGC"
+            or codon == "CGA"
+            or codon == "CGG"
+        ):
+            return "Arg (R)"
+        elif codon == "GGU" or codon == "GGC" or codon == "GGA" or codon == "GGG":
+            return "Gly (G)"
+
+    result = []
+    for i in range(0, len(mrna_seq), 3):
+        result.append(translate(mrna_seq[i : i + 3]))
+
+    return " - ".join(result)
+
+
 def aminoacid_to_codon(amino_acid: str) -> str or None:
     tac = {
         "A": [
@@ -162,12 +235,13 @@ def aminoacid_to_codon(amino_acid: str) -> str or None:
 input_dna = "TTACGA"
 dna_compliment = compliment(input_dna)
 mrna = dnaseq_to_mrna(input_dna)
+aminoacid = mrna_to_aminoacid(mrna)
 
 print(f"Input DNA = {input_dna}")
 print()
 print(f"Compliment DNA = {dna_compliment}")
 print(f"mRNA = {mrna}")
-print(f"Aminoacid =")
+print(f"Aminoacid = {aminoacid}")
 print()
 
 input_aminoacid = "NAN"
